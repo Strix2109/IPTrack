7eb6339048feaab730ccc24b68532d17c47fa39f
STRIX.D <70028787+Strix2109@users.noreply.github.com>
2021-05-14 11:54:33 +0530

    Update ipgeolocation.py

--- ipgeolocation.py
+++ ipgeolocation.py
@@ -1,29 +1,16 @@
 #!/usr/bin/env python3
 # encoding: UTF-8
 
-"""
-    This file is part of IPGeoLocation tool.
-    Copyright (C) 2015-2016 @maldevel
-    https://github.com/maldevel/IPGeoLocation
-    
-    IPGeoLocation - Retrieve IP Geolocation information 
-    Powered by http://ip-api.com
-    
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    
-    For more see the file 'LICENSE' for copying permission.
-"""
+def bann_text():
+    clr()
+    logo = """
+ ____ _____ ____  _____  __  ____   ___  __  __ ____
+/ ___|_   _|  _ \|_ _\ \/ / | __ ) / _ \|  \/  | __ )
+\___ \ | | | |_) || | \  /  |  _ \| | | | |\/| |  _ \
+ ___) || | |  _ < | | /  \  | |_) | |_| | |  | | |_) |
+|____/ |_| |_| \_\___/_/\_\ |____/ \___/|_|  |_|____/
+By STRIX.D
+                                         """
 
 __author__  = 'maldevel'
 
