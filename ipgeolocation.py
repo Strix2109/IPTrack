4d051867637e732a0d65b6dae9c21cea8f7a67b7
STRIX.D <70028787+Strix2109@users.noreply.github.com>
2021-05-14 12:07:02 +0530

    Update ipgeolocation.py

--- ipgeolocation.py
+++ ipgeolocation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # encoding: UTF-8
 
-def bann_text():
-    clr()
-    logo = """
+"""
+    IPTrack tool.
+    https://github.com/Strix2109/IPTrack.git
+    
  ____ _____ ____  _____  __   ___ ____
 / ___|_   _|  _ \|_ _\ \/ /  |_ _|  _ \
 \___ \ | | | |_) || | \  /    | || |_) |
@@ -13,7 +14,10 @@ def bann_text():
 By STRIX CYBER COMMUNITY
 Instagram- @strix_21
 Youtube- STRIX.D
-                                         """
+
+    IPGeoLocation - Retrieve IP Geolocation information 
+    Powered by http://ip-api.com
+"""
 
 __author__  = 'Santkr.'
 
